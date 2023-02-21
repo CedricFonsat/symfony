79c9fef7f0fe5737126da8f92ca1dd2cf431c98c
Cédric FONSAT <fonsat.pro@gmail.com>
2023-02-21 10:27:31 +0000

    Doctrine

--- guestbook/src/Controller/ConferenceController.php
+++ guestbook/src/Controller/ConferenceController.php
@@ -9,25 +9,15 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
-    #[Route('/hello/{name}', name: 'homepage')]
-    public function index(string $name = ''): Response
+    #[Route('/', name: 'homepage')]
+    public function index(Request $request): Response
     {
-        // return $this->render('conference/index.html.twig', [
-        //     'controller_name' => 'ConferenceController',
-        // ]);
-
-        $greet = '';
-               
-                if ($name) {
-                     $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
-                }
-            
+        dd($request);
 
         return new Response(<<<EOF
                    <html>
-                    <body>$greet<img src="/images/under-construction.gif" /></body>
+                   <body><img src="/images/under-construction.gif" /></body>
                    </html>
                EOF);
-            
-    
-}}
+    }
+}
